47c1e323ba236aa7a30044abca80ee438da701ba
Dieter Mutiz <dieter_md@hotmail.com>
2023-05-18 22:14:46 -0300

    Roll dice

--- src/App.jsx
+++ src/App.jsx
@@ -3,20 +3,29 @@ import Die from './components/Die'
 
 
 export default function App() {
+
+  const [dice, setDice] = useState(allNewDice())
+
+  function allNewDice() {
+    const newDice = []
+      for (let i = 0; i < 10; i++) {
+        newDice.push(Math.ceil(Math.random() * 6))
+      }
+      return newDice
+  }
+
+  function rollDice() {
+    setDice(allNewDice())
+  }
+
+  const dieElements = dice.map((dieValue, i) => <Die value={dieValue} key={i} />)
+
   return (
     <main>
       <div className="dice-container">
-        <Die value="1"/>
-        <Die value="1"/>
-        <Die value="1"/>
-        <Die value="1"/>
-        <Die value="1"/>
-        <Die value="1"/>
-        <Die value="1"/>
-        <Die value="1"/>
-        <Die value="1"/>
-        <Die value="1"/>
+        {dieElements}
       </div>
+      <button className="roll-dice" onClick={rollDice}>Roll</button>
     </main>
   )
 }
