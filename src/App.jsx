64f595bc51a66353a314c79d2cc7057919a2bb1e
Dieter Mutiz <dieter_md@hotmail.com>
2023-05-20 21:25:24 -0300

    Hold die

--- src/App.jsx
+++ src/App.jsx
@@ -6,22 +6,40 @@ export default function App() {
 
   const [dice, setDice] = useState(allNewDice())
 
+  function generateNewDie() {
+    return { value: Math.ceil(Math.random() * 6), isHeld: false }
+  }
+
   function allNewDice() {
     const newDice = []
       for (let i = 0; i < 10; i++) {
-        newDice.push(Math.ceil(Math.random() * 6))
+        newDice.push(generateNewDie())
       }
       return newDice
   }
 
   function rollDice() {
-    setDice(allNewDice())
+    setDice(prevDice => prevDice.map(die => 
+        die.isHeld ? die : generateNewDie()
+      )
+    )
   }
 
-  const dieElements = dice.map((dieValue, i) => <Die value={dieValue} key={i} />)
+  function holdDie(index) {
+    setDice(prevDice => {
+      prevDice[index].isHeld = true
+      return [...prevDice]
+    })
+  }
+
+  const dieElements = dice.map((die, i) => 
+    <Die value={die.value} isHeld={die.isHeld} holdDie={() => holdDie(i)} key={i} />
+  )
 
   return (
     <main>
+      <h1 className="title">Tenzies</h1>
+      <p className="instructions">Roll until all dice are the same. Click each die to freeze it at its current value between rolls.</p>
       <div className="dice-container">
         {dieElements}
       </div>
--- src/components/Die.jsx
+++ src/components/Die.jsx
@@ -1,8 +1,12 @@
 import React from "react";
 
 export default function Die(props) {
+  const style = {
+    backgroundColor: props.isHeld ? "#59E391" : "white"
+  }
+
   return (
-    <div className="die-face">
+    <div className="die-face" style={style} onClick={props.holdDie}>
       <h2 className="die-value">{props.value}</h2>
     </div>
   )
