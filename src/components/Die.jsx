366e4467c5b23af9a9cb37c77d6ee8e73eb7ede4
Dieter Mutiz <dieter_md@hotmail.com>
2023-06-18 23:33:46 -0300

    Css die faces

--- src/components/Die.jsx
+++ src/components/Die.jsx
@@ -5,9 +5,11 @@ export default function Die(props) {
     backgroundColor: props.isHeld ? "#59E391" : "white"
   }
 
+  const pips = Array(props.value).fill(<span className="die-pip"></span>)
+
   return (
     <div className="die-face" style={style} onClick={props.holdDie}>
-      <h2 className="die-value">{props.value}</h2>
+      {pips}
     </div>
   )
 }
